c95025ad1588257ca576b256ca03bb5fe8899239
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-08-16 16:58:27 +0300

    Add fix to syntax and add join table for many-to-many relations

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,18 +3,19 @@ CREATE TABLE "medical_histories"(
     "admited_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
     "patient_id" INTEGER NOT NULL,
     "status" VARCHAR(255) NOT NULL,
-    "medical_histories" ADD PRIMARY KEY("id");
+    ADD PRIMARY KEY("id")
 );
 CREATE TABLE "patients"(
     "id" INTEGER NOT NULL,
     "name" VARCHAR(255) NOT NULL,
-    "date_of_birth" DATE NOT NULL
+    "date_of_birth" DATE NOT NULL,
+    ADD PRIMARY KEY("id")
 );
 CREATE TABLE "treatments"(
     "id" INTEGER NOT NULL,
     "type" VARCHAR(255) NOT NULL,
     "name" VARCHAR(255) NOT NULL,
-    "patients" ADD PRIMARY KEY("id");
+    ADD PRIMARY KEY("id")
 );
 CREATE TABLE "invoices"(
     "id" INTEGER NOT NULL,
@@ -23,7 +24,7 @@ CREATE TABLE "invoices"(
     "paid_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
     "medical_history_id" INTEGER NOT NULL,
     "column_6" INTEGER NOT NULL,
-    "invoices" ADD PRIMARY KEY("id");
+    ADD PRIMARY KEY("id")
 );
     
 CREATE TABLE "invoice_items"(
@@ -33,7 +34,7 @@ CREATE TABLE "invoice_items"(
     "total_price" DECIMAL(8, 2) NOT NULL,
     "invoice_id" INTEGER NOT NULL,
     "treatment_id" INTEGER NOT NULL,
-    "invoice_items" ADD PRIMARY KEY("id");
+    ADD PRIMARY KEY("id")
 );
 
 ALTER TABLE
@@ -44,3 +45,10 @@ ALTER TABLE
     "invoice_items" ADD CONSTRAINT "invoice_items_treatment_id_foreign" FOREIGN KEY("treatment_id") REFERENCES "treatments"("id");
 ALTER TABLE
     "invoice_items" ADD CONSTRAINT "invoice_items_invoice_id_foreign" FOREIGN KEY("invoice_id") REFERENCES "invoices"("id");
+
+CREATE TABLE "medical_treatments" (
+    "treatment_id" INTEGER,
+    "medical_history_id" INTEGER,
+    ADD CONSTRAINT "invoice_items_treatment_id_foreign" FOREIGN KEY("treatment_id") REFERENCES "treatments"("id");
+    ADD CONSTRAINT "invoices_medical_history_id_foreign" FOREIGN KEY("medical_history_id") REFERENCES "medical_histories"("id");
+);
